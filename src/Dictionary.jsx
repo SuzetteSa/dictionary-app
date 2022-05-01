bc4f0d72143087d1e83d578447550e2094079d7d
SuzetteSa <89647867+SuzetteSa@users.noreply.github.com>
2022-05-01 15:24:41 -0500

    added definition mapping

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,32 +1,35 @@
 import React, {useState} from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
-let [keyword,setKeyword] = useState("");
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
 
-function handleResponse(response){
-console.log(response.data[0]);
-}
+  //(response.data[0].meanings[0].definitions[0].definition);
 
-
-function search(event){
+  function search(event) {
     event.preventDefault();
-    
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse)
-}
 
-function handleKeywordChange(event){
- setKeyword(event.target.value);
-}
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
 
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-          <input type="search" onChange={handleKeywordChange}/>
+        <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results}/>
     </div>
   );
 }
