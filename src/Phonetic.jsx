fa706c9f606d7ba1d6a4fd2352e84d7936a8bbc5
SuzetteSa <89647867+SuzetteSa@users.noreply.github.com>
2022-05-01 18:19:54 -0500

    added no referrer

--- src/Phonetic.jsx
+++ src/Phonetic.jsx
@@ -3,11 +3,13 @@ import React from "react";
 export default function Phonetic(props) {
     
 return (
-    <div className="Phonetic"> 
-    <a href={props.phonetic.audio} target="_blank">Listen</a>
-    <br/>
+  <div className="Phonetic">
+    <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+      Listen
+    </a>
+    <br />
     {props.phonetic.text}
-    </div>
+  </div>
 );
 
 }
