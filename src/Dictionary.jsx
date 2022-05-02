93d8ca39dac7c29bb3a5225406b04123b706f75a
SuzetteSa <89647867+SuzetteSa@users.noreply.github.com>
2022-05-01 20:59:54 -0500

    styling

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -34,9 +34,11 @@ export default function Dictionary(props) {
   if (loaded) {
       return (
     <div className="Dictionary">
+        
       <section>
+          <h1>What word do you want to look for?</h1>
         <form onSubmit={handleSubmit}>
-          <input type="search" onChange={handleKeywordChange} />
+          <input type="search" onChange={handleKeywordChange} defaultValue={props.defaultKeyword}/>
         </form>
         <div className="hint">Search for a word.....</div>
       </section>
--- src/Meaning.jsx
+++ src/Meaning.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 import Synonyms from "./Synonyms";
+import "./Meaning.css";
 
 export default function Meaning(props) {
-   return (
+  return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <div>
-              <strong>Definition: </strong>
-              {definition.definition}
-              <br />
-              <strong>Example: </strong>
-              <em>{definition.example}</em>
-              <br />
-              <Synonyms synonyms={definition.synonyms}/>
-            </div>
+            <div className="definition">{definition.definition}</div>
+            <div className="example">{definition.example}</div>
+            <Synonyms synonyms={definition.synonyms} />
           </div>
         );
       })}
