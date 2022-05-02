e2cc33f3e0294b7905a9342b361b5a2de4dd6fd2
SuzetteSa <89647867+SuzetteSa@users.noreply.github.com>
2022-05-01 22:56:48 -0500

    photos

--- src/Dictionary.jsx
+++ src/Dictionary.jsx
@@ -1,20 +1,34 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
 
+  function pexelsHandleResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function Search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    let pexelsApiKey =
+      "563492ad6f9170000100000187850b6b0334487d89a4aa781445f2b9";
+      let pexelsApiUrl =
+        `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+      let headers= { Authorization: `Bearer ${pexelsApiKey}` }
+
+         axios.get(pexelsApiUrl, {headers: headers,}).then(pexelsHandleResponse);
   }
 
   function handleSubmit(event) {
@@ -25,7 +39,7 @@ export default function Dictionary(props) {
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-
+//({headers: {"Authorization": `Beaere ${tokenStr}`} });
   function load() {
       setLoaded(true);
        Search();
@@ -43,6 +57,7 @@ export default function Dictionary(props) {
         <div className="hint">Search for a word.....</div>
       </section>
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 
