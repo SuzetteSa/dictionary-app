92776609c9f97f66219e4c4fa36b0204d4876d3d
SuzetteSa <89647867+SuzetteSa@users.noreply.github.com>
2022-08-01 15:07:31 -0500

    cleaning up

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,6 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
-        </header>
         <main>
           <Dictionary defaultKeyword="sunset" />
         </main>
